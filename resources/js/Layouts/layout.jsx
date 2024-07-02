6d3ae5ce4e9788ea03ef436e9d522d58982b2603
ishu10526 <ishu10526@gmail.com>
2024-07-02 17:38:00 +0530

    Add Layout

---
+++ resources/js/Layouts/layout.jsx
@@ -0,0 +1,18 @@
+export default function Layout({children}) {
+    return (
+        <>
+            <header>
+                <nav className="bg-gray-800 pb-10">
+                    <a  href="" className="p-4 bg-gray-400">Home</a>
+                    <a  href="" className="p-4 bg-gray-400"> List</a>
+                    <a  href="" className="p-4 bg-gray-400">Create</a>
+                    <a  href="" className="p-4 bg-gray-400">Edit</a>
+                </nav>
+            </header>
+
+            <main>
+                {children}
+            </main>
+        </>
+    )
+}
